7ad65cd41b7f634586770e25d563ff8700047bf7
Amogh Palnitkar <amogh@mavericklabs.net>
2019-10-30 18:03:24 +0530

    fixed menu shadows

--- src/organisation/OrganisationShow.js
+++ src/organisation/OrganisationShow.js
@@ -1,5 +1,12 @@
 import React, { Component, Fragment } from "react";
-import { Card, CardContent, Menu, MenuItem, Button, Grid } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Menu,
+  MenuItem,
+  Button,
+  Grid
+} from "@material-ui/core";
 import renderOrgChartChildren from "./renderOrgChartChildren";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import find from "lodash/find";
@@ -32,7 +39,8 @@ class OrgnisationShow extends Component {
       orgHierarchy: null,
       zeroEdgeNodes: [],
       menuAnchorEl: null,
-      flatttenedHierarchyStructure: []
+      flatttenedHierarchyStructure: [],
+      selectedZeroEdgeNode : null
     };
   }
 
@@ -180,13 +188,13 @@ class OrgnisationShow extends Component {
   };
 
   handleSubmit = () => {
-    const {orgHierarchy,zeroEdgeNodes} = this.state;
+    const { orgHierarchy, zeroEdgeNodes } = this.state;
     const payload = [orgHierarchy].concat(zeroEdgeNodes);
     const ngoKey = localStorage.getItem("ngo_key");
     console.log(payload);
     // return;
-    api.submitOrgHierarchy(payload,ngoKey).then((response) => {  })
-  }
+    api.submitOrgHierarchy(payload, ngoKey).then(response => {});
+  };
 
   render() {
     const {
@@ -194,10 +202,16 @@ class OrgnisationShow extends Component {
       zeroEdgeNodes,
       flatttenedHierarchyStructure
     } = this.state;
-    console.log({ orgHierarchy,flatttenedHierarchyStructure });
+    console.log({ orgHierarchy, flatttenedHierarchyStructure });
     return (
       <div>
-        <div style={{display: "flex",justifyContent: "space-between",alignItems: "center"}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
+          }}
+        >
           <div>
             <h3>View/Edit Organisation</h3>
           </div>
@@ -233,7 +247,10 @@ class OrgnisationShow extends Component {
             <div>
               <div>
                 <h5>Members with no athlete/coach relationship.</h5>
-                {zeroEdgeNodes.length > 0 ? this.renderZeroEdgeNodes() : "-- No members --"}
+                {zeroEdgeNodes.length > 0
+                  ? this.renderZeroEdgeNodes()
+                  : "-- No members --"}
+                {this.renderMenuItems()}
               </div>
             </div>
           </CardContent>
@@ -246,63 +263,71 @@ class OrgnisationShow extends Component {
     const {
       orgHierarchy,
       zeroEdgeNodes,
-      flatttenedHierarchyStructure
     } = this.state;
     return zeroEdgeNodes.map(item => {
       return (
         <Fragment key={item.key}>
-          <Grid container alignItems='center'>
+          <Grid container alignItems="center">
             <Grid item xs={2}>
-
-            {item.label}
+              {item.label}
             </Grid>
             <Grid item xs={2}>
               <Button
                 color="primary"
                 variant="flat"
                 onClick={event => {
-                  this.setState({ menuAnchorEl: event.currentTarget });
+                  this.setState({ menuAnchorEl: event.currentTarget, selectedZeroEdgeNode : item });
                 }}
               >
-                <PersonAddIcon /> <span style={{marginLeft:'5px'}}>Set Parent</span>
+                <PersonAddIcon />{" "}
+                <span style={{ marginLeft: "5px" }}>Set Parent</span>
               </Button>
             </Grid>
           </Grid>
-          <Menu
-            id="simple-menu"
-            anchorEl={this.state.menuAnchorEl}
-            keepMounted
-            open={Boolean(this.state.menuAnchorEl)}
-            onClose={() => {
-              this.setState({ menuAnchorEl: null });
-            }}
-          >
-            {flatttenedHierarchyStructure.map(node => {
-              return (
-                <MenuItem
-                  key={node.key}
-                  onClick={() => {
-                    this.setParentForNode(item.key, node.key);
-                    this.setState({ menuAnchorEl: null });
-                  }}
-                >
-                  {node.label}
-                </MenuItem>
-              );
-            })}
+        </Fragment>
+      );
+    });
+  };
+
+  renderMenuItems = () => {
+    const {
+      flatttenedHierarchyStructure,
+      selectedZeroEdgeNode
+    } = this.state;
+    return (
+      <Menu
+        id="simple-menu"
+        anchorEl={this.state.menuAnchorEl}
+        keepMounted
+        open={Boolean(this.state.menuAnchorEl)}
+        onClose={() => {
+          this.setState({ menuAnchorEl: null });
+        }}
+      >
+        {flatttenedHierarchyStructure.map(node => {
+          return (
             <MenuItem
+              key={node.key}
               onClick={() => {
-                this.setParentForNode(item.key, null);
+                this.setParentForNode(selectedZeroEdgeNode.key, node.key);
                 this.setState({ menuAnchorEl: null });
               }}
             >
-              --Empty--
+              {node.label}
             </MenuItem>
-          </Menu>
-        </Fragment>
-      );
-    })
-  }
+          );
+        })}
+        <MenuItem
+          onClick={() => {
+            this.setParentForNode(selectedZeroEdgeNode.key, null);
+            this.setState({ menuAnchorEl: null });
+          }}
+        >
+          --Empty--
+        </MenuItem>
+      </Menu>
+    );
+  };
 }
 
 export default OrgnisationShow;
